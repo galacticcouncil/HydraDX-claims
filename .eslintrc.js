b6f5ea89341c45759aeb1ae8015d8b73f9f0183b
mckrava <mckrava2@gmail.com>
2021-02-10 15:34:53 +0100

    Update eslinter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
