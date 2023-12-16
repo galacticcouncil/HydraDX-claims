332fbb52cc9d78ed711b0ad3876df31df0a93cb7
mrq <mrq@mrq.cz>
2023-12-16 22:25:40 +0100

    dont tripple balances again

--- src/components/HdxBalanceDetails.vue
+++ src/components/HdxBalanceDetails.vue
@@ -11,7 +11,6 @@
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
 import { getFormattedBalance } from '@/services/utils';
-import { BigNumber } from 'bignumber.js';
 
 export default defineComponent({
   name: 'HdxBalanceDetails',
@@ -27,15 +26,8 @@ export default defineComponent({
     const hdxClaimableAmountFormatted = computed(() => {
       if (!props.ethAccountData.isClaimableHdxAmountZero) {
         return {
-          base: getFormattedBalance(
-            props.ethAccountData.claimableHdxAmount,
-            true
-          ),
-          tripled: new BigNumber(
-            getFormattedBalance(props.ethAccountData.claimableHdxAmount, true)
-          )
-            .multipliedBy(3)
-            .toString(),
+          base: getFormattedBalance(props.ethAccountData.xhdxTotalBalance),
+          tripled: getFormattedBalance(props.ethAccountData.claimableHdxAmount, true)
         };
       }
       return { base: '0', tripled: '0' };
