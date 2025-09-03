1ffed83a29e6c16643c3f741e04e4e4fdddfcd3a
Jakub Pánik <jakub.panik@gmail.com>
2025-09-03 16:07:51 +0800

    .

--- src/services/polkadotUtils.ts
+++ src/services/polkadotUtils.ts
@@ -4,9 +4,7 @@ import BigNumber from 'bignumber.js';
 
 import { web3FromAddress } from '@polkadot/extension-dapp';
 import { u8aToHex } from '@polkadot/util';
-import { Signer } from '@polkadot/api/types';
 import type { EventRecord } from '@polkadot/types/interfaces';
-import type { ISubmittableResult } from '@polkadot/types/types';
 
 import type { ClaimProcessStatus, ApiListeners } from '@/types';
 import { setApiConnection } from '@/services/polkadotApi';
@@ -15,7 +13,7 @@ const keyring = new Keyring();
 
 let polkadotApiInstance: ApiPromise;
 
-const getSinger = async (account: string): Promise<Signer> => {
+const getSigner = async (account: string): Promise<any> => {
   const injector = await web3FromAddress(account);
   return injector.signer;
 };
@@ -47,7 +45,7 @@ export const claimBalance: (
   account: string,
   statusCl: (status: ClaimProcessStatus) => void
 ) => Promise<void> = async (sign, account, statusCl) => {
-  const signer = await getSinger(account);
+  const signer = await getSigner(account);
   let isCompleted = false;
   const nodeErrorMessages = ['Message signature is wrong', 'Nothing to claim'];
 
@@ -59,39 +57,35 @@ export const claimBalance: (
     });
     const claimResponse = await polkadotApiInstance.tx.claims
       .claim(sign)
-      .signAndSend(
-        account,
-        { signer: signer },
-        ({ events, status }: ISubmittableResult) => {
-          events.forEach(({ phase, event: { data, method } }: EventRecord) => {
-            if (!isCompleted && method === 'ExtrinsicFailed') {
-              isCompleted = true;
-              statusCl({
-                inProgress: false,
-                completed: true,
-                resultStatus: 1,
-              });
-            }
-            if (!isCompleted && status.isInBlock && method === 'Claim') {
-              statusCl({
-                inProgress: true,
-                completed: false,
-                resultStatus: 0,
-                processMessage: 'Almost done! Request is already in block.',
-              });
-            }
-            if (!isCompleted && status.isFinalized && method === 'Claim') {
-              isCompleted = true;
-              statusCl({
-                inProgress: false,
-                completed: true,
-                resultStatus: 0,
-                processMessage: '',
-              });
-            }
-          });
-        }
-      )
+      .signAndSend(account, { signer: signer }, ({ events, status }: any) => {
+        events.forEach(({ phase, event: { data, method } }: EventRecord) => {
+          if (!isCompleted && method === 'ExtrinsicFailed') {
+            isCompleted = true;
+            statusCl({
+              inProgress: false,
+              completed: true,
+              resultStatus: 1,
+            });
+          }
+          if (!isCompleted && status.isInBlock && method === 'Claim') {
+            statusCl({
+              inProgress: true,
+              completed: false,
+              resultStatus: 0,
+              processMessage: 'Almost done! Request is already in block.',
+            });
+          }
+          if (!isCompleted && status.isFinalized && method === 'Claim') {
+            isCompleted = true;
+            statusCl({
+              inProgress: false,
+              completed: true,
+              resultStatus: 0,
+              processMessage: '',
+            });
+          }
+        });
+      })
       .catch(e => {
         console.log('error - ', e.message);
 
